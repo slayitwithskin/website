1e8469fbcbda7dd96ed5d82b1d893d67c310136a
DezyNation <dezynationindia@gmail.com>
2022-11-18 21:22:01 +0530

    menu added

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,32 @@ import leaves from '../public/floral-leaves-corner.jpg'
 import herobg from '../public/hero.jpg'
 import herogirl from '../public/woman.png'
 
-import { Flex, Box, Spacer, Menu, MenuButton, MenuList, MenuIcon, MenuItem, Text, MenuDivider, Button, Image, Container, AspectRatio, HStack } from '@chakra-ui/react'
+import { 
+  Flex, 
+  Box, 
+  Spacer, 
+  Menu, 
+  MenuButton, 
+  MenuList, 
+  MenuItem, 
+  Text,
+  Button, 
+  Image, 
+  HStack,
+  Drawer, 
+  DrawerBody,
+  DrawerHeader,
+  DrawerContent, 
+  DrawerCloseButton, 
+  DrawerOverlay,
+  useDisclosure,
+  Accordion,
+  AccordionItem,
+  AccordionPanel,
+  AccordionButton,
+  AccordionIcon
+} from '@chakra-ui/react'
+
 
 import { HiBars3BottomRight, HiOutlineShoppingBag } from 'react-icons/hi2'
 import { BsChevronDown, BsArrowRight, BsEnvelopeFill, BsTelephoneFill, BsInstagram, BsFacebook, BsTwitter, BsWhatsapp } from 'react-icons/bs'
@@ -16,6 +41,8 @@ import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/react-splide/css/sea-green';
 
 const index = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const menuRef = React.useRef()
   return (
     <>
       <Head>
@@ -121,9 +148,185 @@ const index = () => {
             </Flex>
             <Spacer />
             <Button display={['none', 'none', 'block']} rounded={'xl'} bg={"transparent"} border={"1px"} borderColor={"black"} borderTopRightRadius={"0"} _hover={{ bg: "black", color: "white" }} className={styles.cursive}>Book Appointment</Button>
-            <Text fontSize={28} display={['block', 'block', 'none']}><HiBars3BottomRight /></Text>
+            <Text fontSize={28} display={['block', 'block', 'none']} ref={menuRef} onClick={onOpen}><HiBars3BottomRight /></Text>
           </Flex>
         </nav>
+
+        <Drawer
+        isOpen={isOpen}
+        placement={'right'}
+        size={'full'}
+        onClose={onClose}
+        finalFocusRef={menuRef}
+        >
+          <DrawerOverlay />
+            <DrawerContent>
+              <DrawerCloseButton />
+              <DrawerHeader mb={16}>
+                <Text textAlign={'center'} fontSize={24} className={styles.cursive}>Slay it with Skin</Text>
+              </DrawerHeader>
+              <DrawerBody>
+
+                <Accordion defaultIndex={[]} allowMultiple>
+
+                  <AccordionItem border={'none'}>
+                    <h2>
+                      <AccordionButton>
+                        <Box flex='1' textAlign='left'>
+                          Services
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel>
+
+                      <AccordionItem border={'none'}>
+                        <h2>
+                          <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                              Skin Services
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+
+                        <AccordionPanel>
+                          <Link href={'#'}><Text my={2}>Service 1</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 2</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 3</Text></Link>
+                        </AccordionPanel>
+                      </AccordionItem>
+                      
+
+                      <AccordionItem border={'none'}>
+                        <h2>
+                          <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                              Hair Services
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+
+                        <AccordionPanel>
+                          <Link href={'#'}><Text my={2}>Service 1</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 2</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 3</Text></Link>
+                        </AccordionPanel>
+                      </AccordionItem>
+
+                      <AccordionItem border={'none'}>
+                        <h2>
+                          <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                              Misc.
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+
+                        <AccordionPanel>
+                          <Link href={'#'}><Text my={2}>Service 1</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 2</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 3</Text></Link>
+                        </AccordionPanel>
+                      </AccordionItem>
+
+                    </AccordionPanel>
+                  </AccordionItem>
+
+                </Accordion>
+
+                <Link href={'#'}>
+                  <Box flex={1} px={4} py={2}>
+                    <Text>Blogs</Text>
+                  </Box>
+                </Link>
+                <Link href={'#'}>
+                  <Box flex={1} px={4} py={2}>
+                    <Text>Shop</Text>
+                  </Box>
+                </Link>
+
+                <Accordion defaultIndex={[]} allowMultiple>
+
+                  <AccordionItem border={'none'}>
+                    <h2>
+                      <AccordionButton>
+                        <Box flex='1' textAlign='left'>
+                          Dropdown
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel>
+
+                      <AccordionItem border={'none'}>
+                        <h2>
+                          <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                              Skin Services
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+
+                        <AccordionPanel>
+                          <Link href={'#'}><Text my={2}>Service 1</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 2</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 3</Text></Link>
+                        </AccordionPanel>
+                      </AccordionItem>
+                      
+
+                      <AccordionItem border={'none'}>
+                        <h2>
+                          <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                              Hair Services
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+
+                        <AccordionPanel>
+                          <Link href={'#'}><Text my={2}>Service 1</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 2</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 3</Text></Link>
+                        </AccordionPanel>
+                      </AccordionItem>
+
+                      <AccordionItem border={'none'}>
+                        <h2>
+                          <AccordionButton>
+                            <Box flex='1' textAlign='left'>
+                              Misc.
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+
+                        <AccordionPanel>
+                          <Link href={'#'}><Text my={2}>Service 1</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 2</Text></Link>
+                          <Link href={'#'}><Text my={2}>Service 3</Text></Link>
+                        </AccordionPanel>
+                      </AccordionItem>
+
+                    </AccordionPanel>
+                  </AccordionItem>
+
+                </Accordion>
+
+                <Link href={'#'}>
+                  <Button bg={'black'} color={'white'} rounded={0} my={16} w={'full'}>
+                    <Text className={styles.monts}>BOOK APPOINTMENT</Text>
+                  </Button>
+                </Link>
+
+              </DrawerBody>
+            </DrawerContent>
+        </Drawer>
 
         <Flex alignItems={"center"} justifyContent={"space-between"} h={'auto'} bg={'red.100'} wrap={'wrap'}>
           <Flex direction={"column"} justifySelf={'center'} px={['8','16','24']} pt={24} justifyContent={'center'}>
