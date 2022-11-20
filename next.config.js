a52c849aa8169156aa42af18fc58587151515037
DezyNation <dezynationindia@gmail.com>
2022-11-20 23:56:33 +0530

    eslint rule disabled

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
   images: {
     domains: ['localhost:3000', '1000logos.net', 'elcompanies.com', 'logosdownload.com', 'stickpng.com']
   },
-  rules: {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
-  }
 }
 
 module.exports = nextConfig
