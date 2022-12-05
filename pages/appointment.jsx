7a8b2e3982f4ed552adf781970a48158be1e3421
DezyNation <dezynationindia@gmail.com>
2022-12-05 17:32:19 +0530

    added calendar in appointment page

--- pages/appointment.jsx
+++ pages/appointment.jsx
@@ -5,13 +5,15 @@ import Footer from '../hocs/Footer'
 import Head from 'next/head'
 
 import { Flex, Image, Text, Box, HStack, Container, VStack, Button, Input, Textarea, Stack } from '@chakra-ui/react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
-import "flatpickr/dist/themes/material_green.css";
-import Flatpickr from 'react-flatpickr'
+import appointments from './api/appointments'
 
 const Appointment = () => {
-    const [dob, setDob] = useState()
-    const [appointment, setAppointment] = useState()
+    var now = new Date()
+    const [age, setAge] = useState()
+    const [appointment, setAppointment] = useState(now)
     const [gender, setGender] = useState('female')
 
     return (
@@ -40,15 +42,15 @@ const Appointment = () => {
                                     <Button id='male' _hover={{ bg: '#E3CAA5' }} onClick={(e) => { setGender('male'); e.target.classList.toggle('selected') }}>
                                         Male
                                     </Button>
-                                    <Button id='female' _hover={{ bg: '#E3CAA5' }} onClick={(e) => { setGender('female'); e.target.classList.toggle('selected') }}>
+                                    <Button id='female' className='selected' _hover={{ bg: '#E3CAA5' }} onClick={(e) => { setGender('female'); e.target.classList.toggle('selected') }}>
                                         Female
                                     </Button>
                                 </HStack>
                                 <Input type={'hidden'} name={'gender'} value={gender} />
                             </Box>
-                            <Flex direction={'column'} alignItems={'flex-start'} justifyContent={'flex-start'} pt={4} pl={[0,8]}>
-                                <Text color={'rgb(100,100,100)'} pb={2}>Date of Birth</Text>
-                                <Input id='flatpickr' w={'full'} size={'md'} type={'date'} placeholder={'Select Date'} />
+                            <Flex w={'full'} direction={'column'} alignItems={'flex-start'} justifyContent={'flex-start'} pt={4} pl={[0,8]}>
+                                <Text color={'rgb(100,100,100)'} pb={2}>Your Age</Text>
+                                <Input w={'full'} type={'tel'} value={age} placeholder={'e.g, 21'} onChange={(e)=>setAge(e.target.value)} />
                             </Flex>
                         </Stack>
                     </VStack>
@@ -57,16 +59,36 @@ const Appointment = () => {
                         <HStack w={'full'} my={2}>
                             <Box w={'full'}>
                                 <Text color={'rgb(100,100,100)'} pb={2}>Your Preferred Date of Appointment</Text>
-                                <Input className='flatpickr' w={'full'} size={'md'} type={'date'} placeholder={'Select Appointment Date'} />
+                                <DatePicker 
+                                selected={appointment}
+                                onChange={date => setAppointment(date)}
+                                minDate={now.setDate(now.getDate() + 1)}
+                                maxDate={now.setDate(now.getDate() + 15)}
+                                className='datepicker'
+                                dateFormat={'dd MMMM yyyy'}
+                                withPortal
+                                />
                             </Box>
                         </HStack>
                         <Box w={'full'} pt={4}>
                             <Text color={'rgb(100,100,100)'}>Select Your Time Slot(s)</Text>
                             <Flex w={'full'} wrap={'wrap'} alignItems={'center'} justifyContent={['center', 'flex-start']}>
-                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }}>09:00 - 09:30</Button>
-                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }}>09:00 - 09:30</Button>
-                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }}>09:00 - 09:30</Button>
-                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }}>09:00 - 09:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'1'} disabled={false}>11:00 - 11:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'2'} disabled={false}>11:30 - 12:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'3'} disabled={true}>12:00 - 12:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'4'} disabled={true}>12:30 - 13:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'5'} disabled={false}>13:00 - 13:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'6'} disabled={false}>13:30 - 14:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'7'} disabled={true}>14:00 - 14:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'8'} disabled={true}>14:30 - 15:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'9'} disabled={false}>15:00 - 15:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'10'} disabled={false}>15:30 - 16:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'11'} disabled={false}>16:00 - 16:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'12'} disabled={true}>16:30 - 17:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'13'} disabled={false}>17:00 - 17:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'14'} disabled={false}>17:30 - 18:00</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'15'} disabled={true}>18:00 - 18:30</Button>
+                                <Button m={[2,3]} className={'timeslot'} _hover={{ bg: '#E3CAA5' }} onClick={(e) => { e.target.classList.toggle('selected') }} id={'16'} disabled={true}>18:30 - 19:00</Button>
                             </Flex>
                         </Box>
                     </VStack>
