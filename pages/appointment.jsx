e3f80d6037e3065f6b142b3de8e5bba2fa2c75b3
DezyNation <dezynationindia@gmail.com>
2023-01-03 19:08:33 +0530

    mailer url updated

--- pages/appointment.jsx
+++ pages/appointment.jsx
@@ -89,7 +89,7 @@ const Appointment = () => {
 
     const getAppointmenSlots = async () => {
         const timeslots = document.querySelectorAll('.timeslot')
-        await fetch(`${process.env.BASE_URL}/api/getslots`, {
+        await fetch("/api/getslots", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
